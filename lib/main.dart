92bc5875e7f006abe46d86904aa8a2ee5cc8fe4b
Omar Negm Test <omar1248negm@gmail.com>
2025-03-26 15:03:22 +0200

    add debug mode bunner

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
